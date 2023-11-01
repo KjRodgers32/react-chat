51652e6c2b79805f616df75a338d22e77b3edd2d
Kevin <kjrodgers26@gmail.com>
2023-11-01 15:29:28 -0400

    Removed used imports from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home/Home";
-import PersonIcon from "@mui/icons-material/Person";
 
 const App = () => {
 	return (
